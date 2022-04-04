5ebd569b980494b7075bb6d8356b81d5b0d618d3
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2022-04-04 11:28:02 -0500

    finished javabeans exercise

---
+++ src/main/java/models/Quote.java
@@ -0,0 +1,35 @@
+package models;
+
+import java.io.Serializable;
+
+public class Quote implements Serializable {
+    private int id;
+    private String content;
+    private int authorId;
+
+    public Quote() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(int authorId) {
+        this.authorId = authorId;
+    }
+}
